76e3d8133c1bae09eae0a56bc04960ddd749d435
ozerngg <naze.ozer@robotistan.com>
2022-09-02 16:48:03 +0300

    initial commit

---
+++ Examples/Pre-Installation Code/Pre-Installation(2).py
@@ -0,0 +1,83 @@
+from utime import sleep
+import time
+from machine import Pin, I2C, PWM, ADC
+from picobricks import SSD1306_I2C, NeoPixel, DHT11
+import framebuf
+import random
+
+WIDTH  = 128   # oled display width
+HEIGHT = 64    # oled display height
+
+def button_push(event):
+    if button.value() == 1:
+        oled.text("BUTTON : 1", 0, 10)
+        oled.show()        
+        motor_1.high()
+        motor_2.high()
+        time.sleep(0.5)
+        motor_1.low()
+        motor_2.low()
+        
+i2c = I2C(0, scl=Pin(5), sda=Pin(4), freq=200000)   # Init I2C using pins (default I2C0 pins)
+oled = SSD1306_I2C(WIDTH, HEIGHT, i2c, addr=0x3c)   # Init oled display
+buzzer = PWM(Pin(20))
+buzzer.duty_u16(0)  
+relay = Pin(12, Pin.OUT)
+button = Pin(10, Pin.IN)
+motor_1 = Pin(21, Pin.OUT)
+motor_2 = Pin(22, Pin.OUT)
+pot = ADC(26)
+light_level = ADC(27)
+conversion_factor = 3.3 / (65535) 
+dht_sensor = DHT11(Pin(11, Pin.OUT, Pin.PULL_DOWN))
+led = Pin(7, Pin.OUT)
+neo = NeoPixel(6, n=1, brightness=0.4, autowrite=False)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+COLORS = (RED, GREEN, BLUE)
+
+for color in COLORS:
+        neo.fill(color)
+        neo.show()
+        time.sleep(0.1)
+neo.fill((0,0,0))
+neo.show()
+
+buzzer.duty_u16(2000)
+buzzer.freq(831)
+time.sleep(0.5)
+buzzer.duty_u16(0)
+time.sleep(0.5)
+relay.high()
+time.sleep(0.5)
+relay.low()
+time.sleep(0.5)
+led.high()
+time.sleep(0.5)
+led.low()
+time.sleep(0.5)
+
+
+dht_read_time = time.time()
+button.irq(trigger=Pin.IRQ_RISING, handler=button_push)
+
+while True:
+    if time.time() - dht_read_time >= 3:
+        dht_read_time = time.time()
+        dht_sensor.measure()
+
+    oled.fill(0)
+    oled.text("POT:      {0:.2f}V".format(pot.read_u16() * conversion_factor),0,20) # round(pot.read_u16() * conversion_factor, 2)
+    oled.text("LIGHT:    {0:.2f}%".format((65535.0 - light_level.read_u16())/650.0),0,30)
+    oled.text("TEMP:     {0:.2f}C".format(dht_sensor.temperature),0,40)
+    oled.text("HUMIDITY: {0:.1f}%".format(dht_sensor.humidity),0,50)
+    oled.show()
+    time.sleep(1)
+    oled.fill(0)
+    
+
+
+
+
+
---
+++ Examples/README.md
@@ -0,0 +1,12 @@
+## Development Environments
+ - ##### [MicroBlocks Block Programming Language](http://microblocks.fun/ "MicroBlocks Block Programming Language")
+ ![](https://microblocks.fun/assets/img/opengraph/microblocks.png)
+ MicroBlocks is a free, Scratch-like blocks programming language for learning physical computing with educational microcontroller boards such as the micro:bit, Adafruit Circuit Playground Express, and many others. MicroBlocks is a live environment. Click on a block and it runs immediately, right on the board. Try out commands. See and graph sensor values in real time. No more waiting for code to compile and download.
+ - ##### [Thonny (MicroPython) IDE for Beginners](http://thonny.org/ "Thonny (MicroPython) IDE for Beginners")
+  ![](https://i0.wp.com/randomnerdtutorials.com/wp-content/uploads/2018/10/programming-micropython-esp32-esp8266.jpg?fit=1280%2C720&quality=100&strip=all&ssl=1)
+ At the heart of Picobricks is the Raspberry Pi Pico. The Thonny Raspberry Pi is a great choice for coding Pico and therefore Picobricks.
+ - ##### [Arduino IDE ](http://www.arduino.cc/en/software "Arduino IDE ")
+ ![](https://i.pinimg.com/originals/d8/31/ab/d831ab6f16f82c542868bbd8b708c828.jpg)
+ Picobricks offers us the opportunity to code with Arduino C. Getting started coding the Raspberry Pi Pico at the heart of Picobricks with the widely used Arduino IDE is pretty easy.
+ ## Useful Information
+ If you add main.py to the name of your code, your code will continue as a loop.
