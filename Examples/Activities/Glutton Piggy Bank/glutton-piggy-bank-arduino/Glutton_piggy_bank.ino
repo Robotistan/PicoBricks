9902ecbcf0d45c09ef19a6eded5fd568e087db79
ozerngg <112697142+ozerngg@users.noreply.github.com>
2022-10-10 16:48:32 +0300

    Update Glutton_piggy_bank.ino

--- Examples/Activities/Glutton Piggy Bank/glutton-piggy-bank-arduino/Glutton_piggy_bank.ino
+++ Examples/Activities/Glutton Piggy Bank/glutton-piggy-bank-arduino/Glutton_piggy_bank.ino
@@ -1,14 +1,15 @@
 #include <Servo.h>
-#define trigPin 14
-#define echoPin 15
+#define trigPin 15
+#define echoPin 14
+//define the libraries
 Servo servo;
 void setup() {
   Serial.begin (9600);
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
-  servo.attach(21);
+  //define the input and output pins
+  servo.attach(21); //define the servo pin
 }
-
 void loop() {
   long duration, distance;
   digitalWrite(trigPin, LOW);
@@ -17,18 +18,16 @@ void loop() {
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
   duration = pulseIn(echoPin, HIGH);
-  distance = (duration/2) / 29.1; 
-  if (distance < 80) {
+  distance = (duration/2) / 29.1;
+  //calculate distance
+  if (distance < 5) {    //if the distance variable is less than 5
     Serial.print(distance);
     Serial.println(" cm");
     servo.write(179);
   }
-
-  else if (distance<180) {
+  else if (distance>5) {   // if the distance variable is greater than 5
     Serial.print(distance);
     Serial.println(" cm");
-    servo.write(100); 
+    servo.write(100);
   }
-  
-
 }
