043ea167d237a5346fa65080204e7a78a5f52a52
ozerngg <112697142+ozerngg@users.noreply.github.com>
2022-10-20 09:31:09 +0300

    Update maze_solver_robot.py

--- Examples/Activities/Maze Solver Robot/maze-solver-robot-micropython/maze_solver_robot.py
+++ Examples/Activities/Maze Solver Robot/maze-solver-robot-micropython/maze_solver_robot.py
@@ -1,20 +1,22 @@
 from machine import Pin
 from utime import sleep
-import utime #gereksiz import
+import utime
+#define libraries
 
 trigger = Pin(15, Pin.OUT)
 echo = Pin(14, Pin.IN)
+#define sensor pins
 
 m1 = Pin(21, Pin.OUT)
 m2 = Pin(22, Pin.OUT)
+#define dc motor pins
 
 m1.low()
 m2.low()
 signaloff = 0
 signalon = 0
 
-def getDistance():  # diger orneklerden getDistance alinabilir. Bu biraz hatali gibi
-
+def getDistance():
    trigger.low()
    utime.sleep_us(2)
    trigger.high()
@@ -27,23 +29,30 @@ def getDistance():  # diger orneklerden getDistance alinabilir. Bu biraz hatali 
    timepassed = signalon - signaloff
    distance = (timepassed * 0.0343) / 2
    return distance
+#calculate distance
+
 measure=0
 while True:
+    
     measure=int(getDistance())
-    m1.high()
-    m2.high()
-    if measure<10:
+    print(measure)
+    if measure>5:
+        m1.high()
+        m2.high()
+        sleep(1) #if the distance is higher than 5, the wheels go straight
+    else:
         m1.low()
         m2.low()
-        sleep(1)
+        sleep(0.5)
         m1.high()
         m2.low()
         sleep(0.5)
         measure=int(getDistance())
-        if measure<10:
+        if measure<5:
             m1.low()
-            m2.low()
-            sleep(1)
-            m1.low()
-            m2.high()
-            sleep(1)
+        m2.low()
+        sleep(0.5)
+        m1.low()
+        m2.high()
+        sleep(0.5)
+        #If the distance is less than 5, wait, move in any direction; if the distance is less than 5, move in the opposite direction
