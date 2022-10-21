6e129bd02af14facb759db2e9246df81a757b863
ozerngg <112697142+ozerngg@users.noreply.github.com>
2022-10-21 12:50:35 +0300

    Update magic_lamp_arduino.ino

--- Examples/Activities/Magic Lamp/magic-lamp-arduino/magic_lamp_arduino.ino
+++ Examples/Activities/Magic Lamp/magic-lamp-arduino/magic_lamp_arduino.ino
@@ -1,24 +1,22 @@
-int x=0; 
-
 void setup() {
   // put your setup code here, to run once:
-  pinMode(16,INPUT);//to access the hardware on the pico
-  pinMode(12,OUTPUT);//initialize digital pin 16 as an INPUT for Sensor
-  digitalWrite(12,LOW);//initialize digital pin 12 as an OUTPUT for Relay
-
+  pinMode(1,INPUT);
+  pinMode(7,OUTPUT);
+  //define the input and output pins
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   
-  //When sensor value is '0', the relay will be '1'
-  if (digitalRead(16)==0){
-    digitalWrite(12,HIGH);
-    x=1;
-  } else{
-    digitalWrite(12,LOW);
-    x=0;
+  
+  Serial.println(digitalRead(1));
+
+  if(digitalRead(1)==1){
+    digitalWrite(7,HIGH);
+    delay(3000);
   }
-
-
+  else{
+    digitalWrite(7,LOW);
+    delay(1000);
+  }
 }
