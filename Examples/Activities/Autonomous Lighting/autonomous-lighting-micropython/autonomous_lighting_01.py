3286979307f7deea68f14c16c0e8ae1add9591c4
Galloant <gallmetzeranton@live.com>
2022-10-24 20:24:12 +0200

    Fixed
    Fixed it so it works

--- Examples/Activities/Autonomous Lighting/autonomous-lighting-micropython/autonomous_lighting_01.py
+++ Examples/Activities/Autonomous Lighting/autonomous-lighting-micropython/autonomous_lighting_01.py
@@ -1,30 +1,26 @@
+from machine import Pin,ADC
 import time
-from machine import Pin, ADC
-from picobricks import  WS2812
-#define the library
+from picobricks import WS2812
 
 ldr = ADC(Pin(27))
 ws = WS2812(6, brightness=0.4)
 #define the input and output pins
 
-#define colors
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
+RED = (255,0,0)
+GREEN = (0,255,0)
+BLUE = (0,0,255)
 
-COLORS = (RED, GREEN, BLUE)
-#RGB color Code
+COLORS = (RED,GREEN,BLUE)
 
-while True:#while loop
-    print(ldr.read_u16()) #print the value of the LDR sensor to the screen.
+while True:
     
-    if(ldr.read_u16()>10000):#let's check the ldr sensor
+    print(ldr.read_u16())
+    
+    if(ldr.read_u16()>20000):
         for color in COLORS:
-            
-            #turn on the LDR
-            ws.pixels_fill(color)
-            ws.pixels_show()
-                
+           ws.fill(color)
+           ws.show()
     else:
-        ws.pixels_fill((0,0,0))  #turn off the RGB
-        ws.pixels_show()
+        ws.fill((0,0,0))
+        ws.show()
+
