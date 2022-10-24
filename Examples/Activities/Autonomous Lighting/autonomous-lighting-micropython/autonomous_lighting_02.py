42834305064d55ac581ceff69fa260ec95abdb52
Galloant <gallmetzeranton@live.com>
2022-10-24 20:18:23 +0200

    fixes
    Fixed errors with it not working.

---
+++ Examples/Activities/Autonomous Lighting/autonomous-lighting-micropython/autonomous_lighting_02.py
@@ -0,0 +1,25 @@
+from machine import Pin,ADC
+import time
+from picobricks import WS2812
+
+ldr = ADC(Pin(27))
+ws = WS2812(6, brightness = 0.4)
+
+RED = (255,0,0)
+GREEN = (0,255,0)
+BLUE = (0,0,255)
+
+COLORS = (RED,GREEN,BLUE)
+
+while True:
+    
+    print(ldr.read_u16())
+    
+    if(ldr.read_u16()>20000):
+        for color in COLORS:
+           ws.fill(color)
+           ws.show()
+    else:
+        ws.fill((0,0,0))
+        ws.show()
+
