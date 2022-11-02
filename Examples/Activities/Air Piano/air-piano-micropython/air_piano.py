6030a874c1b58e9d1e531595e49358da116feae4
ozerngg <112697142+ozerngg@users.noreply.github.com>
2022-11-02 14:45:06 +0300

    Update air_piano.py

--- Examples/Activities/Air Piano/air-piano-micropython/air_piano.py
+++ Examples/Activities/Air Piano/air-piano-micropython/air_piano.py
@@ -1,18 +1,24 @@
 from machine import Pin, PWM, I2C
 from utime import sleep
-import utime        # gereksiz import
-from ssd1306 import SSD1306_I2C  #picobricks kullanalım
+import utime       
+from picobricks import SSD1306_I2C  
 import _thread
+#define the Libraries
 
 buzzer=PWM(Pin(20,Pin.OUT))
 trigger = Pin(15, Pin.OUT)
 echo = Pin(14, Pin.IN)
+#define the input and Output pins
 
 WIDTH  = 128                                            
-HEIGHT = 64                                          
+HEIGHT = 64
+#oled screen settings
+
 sda=machine.Pin(4)
 scl=machine.Pin(5)
 i2c=machine.I2C(0,sda=sda, scl=scl, freq=1000000)
+#initialize digital pin 4 and 5 as an OUTPUT for OLED Communication
+
 oled = SSD1306_I2C(128, 64, i2c)
 
 measure=0
@@ -30,6 +36,7 @@ def getDistance():
    timepassed = signalon - signaloff
    distance = (timepassed * 0.0343) / 2
    return distance
+#calculate distance
 
 def airPiano():
     while True:
@@ -73,6 +80,7 @@ def airPiano():
             buzzer.duty_u16(0)
 
 _thread.start_new_thread(airPiano, ())
+#Play the tone determined by the value of the distance sensor
 
 while True:
     measure=int(getDistance())
@@ -81,3 +89,4 @@ while True:
     sleep(0.01)
     oled.fill(0)
     oled.show()
+#Write the specified texts to the determined x and ye coordinates on the OLED screen
