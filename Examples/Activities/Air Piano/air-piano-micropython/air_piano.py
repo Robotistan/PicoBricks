051f6d5580513b0614056a564fc20f813d78b852
Samet <111511331+amet7@users.noreply.github.com>
2022-11-02 15:44:28 +0300

    Update air_piano.py

--- Examples/Activities/Air Piano/air-piano-micropython/air_piano.py
+++ Examples/Activities/Air Piano/air-piano-micropython/air_piano.py
@@ -19,7 +19,7 @@ scl=machine.Pin(5)
 i2c=machine.I2C(0,sda=sda, scl=scl, freq=1000000)
 #initialize digital pin 4 and 5 as an OUTPUT for OLED communication
 
-oled = SSD1306_I2C(128, 64, i2c)
+oled = SSD1306_I2C(WIDTH, HEIGHT, i2c)
 
 measure=0
 
