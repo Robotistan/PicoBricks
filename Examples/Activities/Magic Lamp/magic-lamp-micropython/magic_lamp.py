0bfef4646890ddd6f39b857e0bd53f8e5968eec0
ozerngg <112697142+ozerngg@users.noreply.github.com>
2022-11-16 14:55:11 +0300

    Update magic_lamp.py

--- Examples/Activities/Magic Lamp/magic-lamp-micropython/magic_lamp.py
+++ Examples/Activities/Magic Lamp/magic-lamp-micropython/magic_lamp.py
@@ -1,6 +1,6 @@
 from machine import Pin #to access the hardware on the pico
 sensor=Pin(1,Pin.IN) #initialize digital pin 1 as an INPUT for Sensor
-led=Pin(7,Pin.OUT)#initialize digital pin 7 as an OUTPUT for Relay
+led=Pin(7,Pin.OUT)#initialize digital pin 7 as an OUTPUT for LED
 while True:
     #When sensor value is '0', the relay will be '1'
     print(sensor.value())
