9373e67ec07d83ee90cbf4e8f6145c1cda8c3ac6
Mehmet Ali <78815495+mehmetalidag97@users.noreply.github.com>
2023-03-06 10:11:22 +0300

    fixed

--- Software/Activities/Smart Cooler/smart-cooler-micropython/smart_cooler.py
+++ Software/Activities/Smart Cooler/smart-cooler-micropython/smart_cooler.py
@@ -4,17 +4,17 @@ import utime
 
 LIMIT_TEMPERATURE = 20 #define the limit temperature
 
-dht_sensor = DHT11(Pin(11, Pin.IN, Pin.PULL_DOWN))
+dht_sensor = DHT11(Pin(11))
 m1 = Pin(21, Pin.OUT)
 m1.low()
 dht_read_time = utime.time()
 #define input-output pins
 
 while True:
-    if utime.time() - dht_read_time >= 3:
+    if (utime.time() - dht_read_time > 2):
         dht_read_time = utime.time()
         dht_sensor.measure()
-        temp= dht_sensor.temperature
+        temp=dht_sensor.temperature
         print(temp)
         if temp >= LIMIT_TEMPERATURE:     
             m1.high()
