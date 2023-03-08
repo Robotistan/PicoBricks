164501c9b682e51daec772bcedb60fa0dd4bd33c
ozerngg <112697142+ozerngg@users.noreply.github.com>
2023-03-08 11:12:07 +0300

    Update voice_controlled_robot_car.py

--- Software/Activities/Voice Controlled Robot Car/voice-controlled-robot-car-micropython/voice_controlled_robot_car.py
+++ Software/Activities/Voice Controlled Robot Car/voice-controlled-robot-car-micropython/voice_controlled_robot_car.py
@@ -4,24 +4,26 @@ from utime import sleep
 uart = UART(0,9600) #If connection cannot be established, try 115200.
 m1 = Pin(21, Pin.OUT)
 m2 = Pin(22, Pin.OUT)
+cmd = uart.readline()
 
 m1.low()
 m2.low()
-
+cmd = ""
 while True:
     sleep(0.05)
     if uart.any():
         cmd = uart.readline()
-    if cmd==b'F':
+        print(cmd)
+    if cmd.startswith(b'f') or cmd.startswith(b'F'):
         m1.high()
         m2.high()
-    elif cmd==b'R':
+    elif cmd.startswith(b'r') or cmd.startswith(b'R'):
         m1.high()
         m2.low()
-    elif cmd==b'L':
+    elif cmd.startswith(b'l') or cmd.startswith(b'L'):
         m1.low()
         m2.high()
-    elif cmd==b'S':
+    elif cmd.startswith(b's') or cmd.startswith(b'S'):
         m1.low()
         m2.low()
     cmd=""
