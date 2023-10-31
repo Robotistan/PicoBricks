944d6731608f0a07499dab688f1a250920b88361
Mehmet Ali <78815495+mehmetalidag97@users.noreply.github.com>
2023-10-31 16:06:29 +0300

    Update Ferris Wheel.py

--- PicoBricks Add-On Kits/Ferris Wheel/Ferris Wheel MicroPython/Ferris Wheel.py
+++ PicoBricks Add-On Kits/Ferris Wheel/Ferris Wheel MicroPython/Ferris Wheel.py
@@ -7,13 +7,16 @@ from machine import PWM, Pin, ADC
 pot = ADC(26)
 motor_1 = PWM(Pin(21))
 motor_2 = PWM(Pin(22))
+motor_1.freq(50)
+motor_2.freq(50)
 motor_1.duty_u16(0)
 motor_2.duty_u16(0)
 while True:
     pot_val = pot.read_u16()
     time.sleep(0.2)
-#     print(pot_val)
+    
     if pot_val > 800:
+        print("sa")
         motor_1.duty_u16(pot_val-400)
         motor_2.duty_u16(pot_val-400)
     else:
