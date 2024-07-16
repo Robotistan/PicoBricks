988c9e75c2e1404aec41f30a6312e27bf3c170d4
AtakanOztrk <134678576+AtakanOztrk@users.noreply.github.com>
2024-07-16 12:04:33 +0300

    Create smart_cooler_v1.py

---
+++ Software/Activities/Smart Cooler/smart-cooler-micropython/smart_cooler_v1.py
@@ -0,0 +1,23 @@
+from machine import Pin
+from picobricks import DHT11
+import utime
+
+LIMIT_TEMPERATURE = 20 #define the limit temperature
+
+dht_sensor = DHT11(Pin(11))
+m1 = Pin(21, Pin.OUT)
+m1.low()
+dht_read_time = utime.time()
+#define input-output pins
+
+while True:
+    if (utime.time() - dht_read_time > 2):
+        dht_read_time = utime.time()
+        dht_sensor.measure()
+        temp=dht_sensor.temperature
+        print(temp)
+        if temp >= LIMIT_TEMPERATURE:     
+            m1.high()
+            #operate if the room temperature is higher than the limit temperature
+        else:
+            m1.low()
