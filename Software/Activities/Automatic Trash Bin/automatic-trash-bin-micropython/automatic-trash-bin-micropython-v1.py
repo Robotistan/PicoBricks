03a5bd078362ffb940f272f0858e5c1404c0e2ed
AtakanOztrk <134678576+AtakanOztrk@users.noreply.github.com>
2024-07-16 10:42:07 +0300

    Create automatic-trash-bin-micropython-v1.py

---
+++ Software/Activities/Automatic Trash Bin/automatic-trash-bin-micropython/automatic-trash-bin-micropython-v1.py
@@ -0,0 +1,32 @@
+from machine import Pin, PWM
+import utime
+
+servo=PWM(Pin(21,Pin.OUT))
+trigger = Pin(15, Pin.OUT)
+echo = Pin(14, Pin.IN)
+
+servo.freq(50)
+servo.duty_u16(1920) #15 degree
+
+def getDistance():
+   trigger.low()
+   utime.sleep_us(2)
+   trigger.high()
+   utime.sleep_us(5)
+   trigger.low()
+   while echo.value() == 0:
+       signaloff = utime.ticks_us()
+   while echo.value() == 1:
+       signalon = utime.ticks_us()
+   timepassed = signalon - signaloff
+   distance = (timepassed * 0.0343) / 2
+   print("The distance from object is ",distance,"cm")
+   return distance
+
+while True:
+    utime.sleep(0.01)
+    if int(getDistance())<=10:
+        servo.duty_u16(4010) #70 degree
+        utime.sleep(1)
+    else:
+        servo.duty_u16(1920)
