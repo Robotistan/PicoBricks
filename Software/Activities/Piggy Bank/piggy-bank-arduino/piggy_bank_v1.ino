ee8e0e45e4a8cdcc0e2a8276028a685a86a250d1
AtakanOztrk <134678576+AtakanOztrk@users.noreply.github.com>
2024-10-21 14:38:58 +0300

    Create piggy_bank_v1.ino

---
+++ Software/Activities/Piggy Bank/piggy-bank-arduino/piggy_bank_v1.ino
@@ -0,0 +1,33 @@
+#include <Servo.h>
+#define trigPin 15
+#define echoPin 14
+
+Servo servo;
+
+void setup() {
+  Serial.begin (9600);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  servo.attach(21); //define the servo pin
+}
+void loop() {
+  long duration, distance;
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration/2) / 29.1;
+  //calculate distance
+  if (distance < 5) {    //if the distance variable is less than 5
+    Serial.print(distance);
+    Serial.println(" cm");
+    servo.write(179);
+  }
+  else if (distance>5) {   // if the distance variable is greater than 5
+    Serial.print(distance);
+    Serial.println(" cm");
+    servo.write(100);
+  }
+}
