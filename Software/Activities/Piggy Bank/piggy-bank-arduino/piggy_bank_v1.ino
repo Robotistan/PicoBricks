f3d35c4baee3b5cf7b5d527bfd3b5aa2e295f6cc
AtakanOztrk <134678576+AtakanOztrk@users.noreply.github.com>
2025-04-18 13:28:48 +0300

    Update piggy_bank_v1.ino

--- Software/Activities/Piggy Bank/piggy-bank-arduino/piggy_bank_v1.ino
+++ Software/Activities/Piggy Bank/piggy-bank-arduino/piggy_bank_v1.ino
@@ -1,33 +1,42 @@
-#include <Servo.h>
-#define trigPin 15
-#define echoPin 14
+#include <picobricks.h>
 
-Servo servo;
+// Define ultrasonic sensor pins
+#define echoPin 14           // Echo pin of the ultrasonic sensor (HC-SR04)
+#define trigPin 15           // Trigger pin of the ultrasonic sensor
+
+// Define the pin connected to the servo motor
+#define MOTOR_1 21
+
+// Create a ServoSimple object to control the servo on pin 21
+ServoSimple Servo(MOTOR_1);
 
 void setup() {
-  Serial.begin (9600);
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);
-  servo.attach(21); //define the servo pin
+  pinMode(trigPin, OUTPUT);  // Set trigger pin as output
+  pinMode(echoPin, INPUT);   // Set echo pin as input
+  Servo.begin();             // Initialize the servo motor
 }
+
 void loop() {
   long duration, distance;
+
+  // Trigger the ultrasonic sensor by sending a 10µs pulse
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
+
+  // Read the duration of the echo pulse
   duration = pulseIn(echoPin, HIGH);
-  distance = (duration/2) / 29.1;
-  //calculate distance
-  if (distance < 5) {    //if the distance variable is less than 5
-    Serial.print(distance);
-    Serial.println(" cm");
-    servo.write(179);
+
+  // Convert the time into a distance in centimeters
+  distance = (duration / 2) / 29.1;
+
+  // If an object is detected closer than 5 cm
+  if (distance < 5) {
+    Servo.setAngle(179);  // Open or move servo to max angle
   }
-  else if (distance>5) {   // if the distance variable is greater than 5
-    Serial.print(distance);
-    Serial.println(" cm");
-    servo.write(100);
+  else {
+    Servo.setAngle(100);  // Set servo to default position
   }
 }
