604ad6ed5f4e1c5bd0a54186bde21a761bc96760
AtakanOztrk <134678576+AtakanOztrk@users.noreply.github.com>
2026-04-02 14:00:18 +0300

    Update moneybox_v2.py

--- PicoBricks Add-On Kits/Money Box/Money Box Micropython/moneybox_v2.py
+++ PicoBricks Add-On Kits/Money Box/Money Box Micropython/moneybox_v2.py
@@ -1,6 +1,8 @@
 from machine import Pin, PWM, I2C
 from picobricks import MotorDriver
 import utime
+import bluetooth
+from ble_simple_peripheral import BLESimplePeripheral
 
 CLOSED_POSITION = 110
 OPEN_POSITION = 10
@@ -12,6 +14,16 @@ trigger = Pin(15, Pin.OUT)
 echo = Pin(14, Pin.IN)
 
 motor.servo(1,CLOSED_POSITION)
+
+MAX_LEN = 10
+buffer = bytearray()
+ble_data = bytearray(5)
+ble_data[0] = 72
+ble_data[1] = 2
+ble_data[2] = 3
+
+ble = bluetooth.BLE()
+sp = BLESimplePeripheral(ble)
 
 trashDetected = 0
 distance = 100
@@ -29,20 +41,47 @@ def getDistance():
    timepassed = signalon - signaloff
    distance = (timepassed * 0.0343) / 2
    utime.sleep(0.1)
-   print(distance,"cm")
+#    print(distance,"cm")
    return distance
 
+def on_rx(data):
+    global buffer
+
+    for b in data:
+        buffer.append(b)
+        if len(buffer) > MAX_LEN:
+            buffer = buffer[1:]
+
+#     print("BUFFER:", buffer)
+
+sp.on_write(on_rx)
+
 while True:
-    rawDistance = getDistance()
-    if rawDistance < 1200:
-        distance = rawDistance
-    motor.servo(1,CLOSED_POSITION)
-    if (distance < 9):
-        trashDetected = 1
-        utime.sleep(0.3)
-    
-    if (distance > 13) and trashDetected == 1:
-        trashDetected = 0
-        motor.servo(1,OPEN_POSITION)
+    if sp.is_connected():
+        distance = int(getDistance())
+        dist_bytes = distance.to_bytes(2, 'little')
+        ble_data[3] = dist_bytes[0]
+        ble_data[4] = dist_bytes[1]
+        sp.send(bytes(ble_data))
+        if buffer:
+            if (buffer[0] == 72 and buffer[1] == 2 and buffer[2] == 1): #Up
+                motor.servo(1,OPEN_POSITION)
+            elif (buffer[0] == 72 and buffer[1] == 2 and buffer[2] == 2): #Down
+                motor.servo(1,OPEN_POSITION)
+			buffer[:] = b''
         utime.sleep(0.5)
-    utime.sleep(0.5)
+    else:
+        rawDistance = getDistance()
+        if rawDistance < 1200:
+            distance = rawDistance
+        motor.servo(1,CLOSED_POSITION)
+        if (distance < 9):
+            trashDetected = 1
+            utime.sleep(0.3)
+        
+        if (distance > 13) and trashDetected == 1:
+            trashDetected = 0
+            motor.servo(1,OPEN_POSITION)
+            utime.sleep(0.5)
+        utime.sleep(0.5)
+
